98009bb9333c4dbc6168fbf0ce7f2bc498376ced
Afsar Arafath <aarafath@igategegdc.com>
2022-09-07 23:03:45 +0530

    refactor header

--- src/javascript/header.js
+++ src/javascript/header.js
@@ -1,16 +1,10 @@
 import { createButton, createDiv, createTextInput } from "./templates.js";
 import { addListItem, addListItemOnEnter, deleteAllItems, deleteSelectedItems, toggleAllCheckboxes } from "./utils.js";
 
-function createAddButton() {
-    const addButton = createButton({ label: 'Add', id: 'addButton' });
-    addButton.onclick = addListItem;
-    return addButton;
-};
-
 export function createHeader() {
     const headerContainer = createDiv({ id: 'headerContainer' })
     const textInput = createTextInput({ placeholder: 'Type here...', id: 'textInput', onkeyup: addListItemOnEnter })
-    const addButton = createAddButton(textInput);
+    const addButton = createButton({ label: 'Add', id: 'addButton', onclick: addListItem });
     const toggleAllButton = createButton({ label: 'Select/Deselect All', id: 'toggleAllButton', onclick: toggleAllCheckboxes });
     const deleteSelectedButton = createButton({ label: 'Delete Selected', id: 'deleteSelectedButton', onclick: deleteSelectedItems });
     const deleteCompletedButton = createButton({ label: 'Delete Completed', id: 'deleteCompletedButton', onclick: deleteAllItems });
